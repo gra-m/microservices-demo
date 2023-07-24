f302c6b3514b193838a03c0fd8640c5551cb3768
gra-m <grahamd483@gmail.com>
2023-07-24 18:44:10 +0200

    30f. Containerized docker config failing to run

--- docker-compose/check-config-server-started.sh
+++ docker-compose/check-config-server-started.sh
@@ -7,7 +7,7 @@ yes | apt-get install curl
 
 curlResult=$(curl -s -o /dev/null -I -w "%{http_code}" http://config-server:8888/actuator/health)
 
-echo "result status code" $curlResult
+echo "result status code" "$curlResult"
 
 while [[ ! $curlResult == "200" ]]; do
   >&2 echo "Config-server is not up yet!"
