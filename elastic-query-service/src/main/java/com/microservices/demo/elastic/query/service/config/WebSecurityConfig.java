cac44668fbaae724e988ad3215c03d9018eec7c6
gra-m <grahamd483@gmail.com>
2023-08-08 07:16:14 +0200

    47f. temp plain password served from config file

--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/config/WebSecurityConfig.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/config/WebSecurityConfig.java
@@ -7,18 +7,23 @@ package com.microservices.demo.elastic.query.service.config;
  * My First Take (MFT== not researched == all prior comments!) --> Difference with config-server
  * SecurityConfig.class is @EnableWebSecurity as opposed to enabling internal library endpoints for encrypt/decrypt</p>
  */
+import com.microservices.demo.config.UserConfigData;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+private final UserConfigData userConfigData;
 
-  @Override
+public WebSecurityConfig(UserConfigData configData) {
+  this.userConfigData = configData;
+}
+
+@Override
   public void configure(HttpSecurity http) throws Exception {
 
   http
@@ -39,9 +44,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 @Override
 protected void configure(AuthenticationManagerBuilder auth) throws Exception{
     auth.inMemoryAuthentication()
-    .withUser("test")
-    .password("{noop}test1234").roles("USER");
-
+        .withUser(userConfigData.getUsername())
+        .password("{noop}" + userConfigData.getPassword())
+        .roles(userConfigData.getRole());
   }
 
 }
