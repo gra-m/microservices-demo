b33acf27b52f2fb34e64cac5b7951979586e67c5
gra-m <grahamd483@gmail.com>
2023-08-10 05:43:13 +0200

    59b. QueryController and ErrorHandler added

---
+++ elastic-query-web-client/src/main/java/com/microservices/demo/elastic/query/web/client/api/QueryController.java
@@ -0,0 +1,62 @@
+package com.microservices.demo.elastic.query.web.client.api;
+
+import com.microservices.demo.elastic.query.web.client.model.ElasticQueryWebClientRequestModel;
+import com.microservices.demo.elastic.query.web.client.model.ElasticQueryWebClientResponseModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Cannot use @RestController here as it auto converts to json, instead @Controller. This is the gateway api into the
+ * web application
+ */
+@Controller
+public class QueryController {
+private static final Logger LOG = LoggerFactory.getLogger(QueryController.class);
+
+@GetMapping("")
+public String index() {
+    return "index";
+}
+
+@GetMapping("/error")
+public String error() {
+    return "error";
+}
+
+  @GetMapping("/home")
+  public String home(Model model) {
+    model.addAttribute("elasticQueryWebClientRequestModel", ElasticQueryWebClientRequestModel.builder().build());
+    return "home";
+}
+
+@PostMapping("/query-by-text")
+public String queryByText(@Valid ElasticQueryWebClientRequestModel requestModel, Model model) {
+
+    LOG.info("Querying with text: {}", requestModel.getText());
+
+    //Dummy response model created here
+    List<ElasticQueryWebClientResponseModel> responseModels = new ArrayList<>();
+    responseModels.add(ElasticQueryWebClientResponseModel.builder()
+    .id("100")
+    .text(requestModel.getText())
+    .build());
+
+    model.addAttribute("elasticQueryWebClientRequestModels", responseModels);
+    model.addAttribute("searchText", requestModel.getText());
+    //model.addAttribute("elasticQueryWebClientRequestModel", ElasticQueryWebClientRequestModel.builder().build());
+    model.addAttribute("elasticQueryWebClientRequestModel", requestModel);
+
+    return "home";
+
+
+
+}
+
+}
